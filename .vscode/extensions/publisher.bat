0b17fbfee05ca4273ce4ce16372222c4e6a11557
YukkuriC <799433638@qq.com>
2026-06-07 15:56:37 +0800

    update publisher script

--- .vscode/extensions/publisher.bat
+++ .vscode/extensions/publisher.bat
@@ -2,7 +2,9 @@ cd /d "%~dp0"
 call hexparse_highlight\prebuild.bat
 call hexparse_highlight\build.bat
 cd hexparse_highlight
-vsce package
+call vsce package
 @REM fuck you microsoft I just want an API key for ur dumbass extension
 @REM why u torture me with this shitty azure ad site
-start https://marketplace.visualstudio.com/manage/publishers/yukkuric
+set "URL=https://marketplace.visualstudio.com/manage/publishers/yukkuric"
+echo %URL%
+start %URL%
