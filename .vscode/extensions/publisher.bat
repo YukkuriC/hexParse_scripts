fcf4012047345187a0066bf25b965b9490fceaa5
YukkuriC <799433638@qq.com>
2026-06-07 15:42:46 +0800

    standard-version not restricted, removed

--- .vscode/extensions/publisher.bat
+++ .vscode/extensions/publisher.bat
@@ -2,7 +2,6 @@ cd /d "%~dp0"
 call hexparse_highlight\prebuild.bat
 call hexparse_highlight\build.bat
 cd hexparse_highlight
-call npx standard-version
 vsce package
 @REM fuck you microsoft I just want an API key for ur dumbass extension
 @REM why u torture me with this shitty azure ad site
