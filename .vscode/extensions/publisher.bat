9357e8218b319f7dfa424ce8fc3fbb8b0784257a
YukkuriC <799433638@qq.com>
2026-06-07 14:13:39 +0800

    add standard-version

--- .vscode/extensions/publisher.bat
+++ .vscode/extensions/publisher.bat
@@ -2,5 +2,8 @@ cd /d "%~dp0"
 call hexparse_highlight\prebuild.bat
 call hexparse_highlight\build.bat
 cd hexparse_highlight
+call npx standard-version
 vsce package
-vsce publish *.vsix
+@REM fuck you microsoft I just want an API key for ur dumbass extension
+@REM why u torture me with this shitty azure ad site
+start https://marketplace.visualstudio.com/manage/publishers/yukkuric
